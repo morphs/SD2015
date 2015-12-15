9b1868fedc9e08c60cea8b20a318e53a4ccdffc3
morphs <morps01@gmail.com>
2015-12-15 19:29:44 -0200

    Key manager tela e detalhes

--- ProjetoSD/src/br/edu/ufabc/sd2015/projeto/server/FJob_ServerGUI.java
+++ ProjetoSD/src/br/edu/ufabc/sd2015/projeto/server/FJob_ServerGUI.java
@@ -9,6 +9,9 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Insets;
+import javax.swing.JList;
+import javax.swing.JTextField;
 
 public class FJob_ServerGUI extends JFrame {
 
@@ -45,10 +48,10 @@ public class FJob_ServerGUI extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[]{91, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JButton btnNewJob = new JButton("New job");
@@ -56,9 +59,58 @@ public class FJob_ServerGUI extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
+		
+		JButton btnListclients = new JButton("ListClients");
+		GridBagConstraints gbc_btnListclients = new GridBagConstraints();
+		gbc_btnListclients.insets = new Insets(0, 0, 5, 5);
+		gbc_btnListclients.gridx = 0;
+		gbc_btnListclients.gridy = 1;
+		contentPane.add(btnListclients, gbc_btnListclients);
+		
+		JList lClients = new JList();
+		GridBagConstraints gbc_lClients = new GridBagConstraints();
+		gbc_lClients.insets = new Insets(0, 0, 5, 5);
+		gbc_lClients.fill = GridBagConstraints.BOTH;
+		gbc_lClients.gridx = 3;
+		gbc_lClients.gridy = 1;
+		contentPane.add(lClients, gbc_lClients);
+		
+		JButton btnListjobs = new JButton("List Jobs");
+		btnListjobs.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btnListjobs = new GridBagConstraints();
+		gbc_btnListjobs.insets = new Insets(0, 0, 5, 5);
+		gbc_btnListjobs.gridx = 0;
+		gbc_btnListjobs.gridy = 3;
+		contentPane.add(btnListjobs, gbc_btnListjobs);
+		
+		JList lJobs = new JList();
+		GridBagConstraints gbc_lJobs = new GridBagConstraints();
+		gbc_lJobs.insets = new Insets(0, 0, 5, 5);
+		gbc_lJobs.fill = GridBagConstraints.BOTH;
+		gbc_lJobs.gridx = 3;
+		gbc_lJobs.gridy = 3;
+		contentPane.add(lJobs, gbc_lJobs);
+		
+		JButton btnReadjob = new JButton("Read Job");
+		GridBagConstraints gbc_btnReadjob = new GridBagConstraints();
+		gbc_btnReadjob.insets = new Insets(0, 0, 5, 5);
+		gbc_btnReadjob.gridx = 0;
+		gbc_btnReadjob.gridy = 5;
+		contentPane.add(btnReadjob, gbc_btnReadjob);
+		
+		JButton btnRemoveJob = new JButton("Remove Job");
+		GridBagConstraints gbc_btnRemoveJob = new GridBagConstraints();
+		gbc_btnRemoveJob.insets = new Insets(0, 0, 5, 5);
+		gbc_btnRemoveJob.gridx = 0;
+		gbc_btnRemoveJob.gridy = 6;
+		contentPane.add(btnRemoveJob, gbc_btnRemoveJob);
 		GridBagConstraints gbc_btnNewJob = new GridBagConstraints();
+		gbc_btnNewJob.insets = new Insets(0, 0, 0, 5);
 		gbc_btnNewJob.gridx = 0;
-		gbc_btnNewJob.gridy = 0;
+		gbc_btnNewJob.gridy = 9;
 		contentPane.add(btnNewJob, gbc_btnNewJob);
 	}
 
